743e09d18753c9c7acd368574aa6a2e5a546acbb
Sanjeev5079 <yourpal79@gmail.com>
2016-06-04 19:50:18 +0530

    test comment

--- demoProjects/src/demoProjects/helloWorld.java
+++ demoProjects/src/demoProjects/helloWorld.java
@@ -1,6 +1,7 @@
 package demoProjects;
 
 public class helloWorld {
+	//comment
 	public static void main(String[] args){
 	System.out.println("Hello World");
 	System.out.println("First change to test Git");

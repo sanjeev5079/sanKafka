a494d5a10efe00b84fcb810c7507556b84682959
Sanjeev5079 <yourpal79@gmail.com>
2016-06-05 21:16:37 +0530

    few More changes

--- KafkaProducerConsumer/src/ProducerConsumer/SimpleProducer.java
+++ KafkaProducerConsumer/src/ProducerConsumer/SimpleProducer.java
@@ -1,4 +1,4 @@
-//Eclipse-Git inegration demo
+
 package ProducerConsumer;
 
 import java.io.BufferedReader;
@@ -11,9 +11,9 @@ import kafka.producer.ProducerConfig;
 
 public class SimpleProducer {
 	private static Producer<Integer, String> producer;
-	//using Topic - testt.
+	
     private static final String topic= "testt";
-    //init properties
+    
     public void initialize() {
           Properties producerProps = new Properties();
           producerProps.put("metadata.broker.list", "localhost:9092");
@@ -22,7 +22,7 @@ public class SimpleProducer {
           ProducerConfig producerConfig = new ProducerConfig(producerProps);
           producer = new Producer<Integer,String>(producerConfig);
     }
-    // publish messages
+    
     public void publishMesssage() throws Exception{            
           BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));               
       while (true){

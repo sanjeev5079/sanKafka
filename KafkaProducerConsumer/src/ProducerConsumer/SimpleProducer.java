27045a406fe0dfab25915fe9f0f1ae3f4b706869
Sanjeev5079 <yourpal79@gmail.com>
2016-06-06 22:33:54 +0530

    commnet 6/6

--- KafkaProducerConsumer/src/ProducerConsumer/SimpleProducer.java
+++ KafkaProducerConsumer/src/ProducerConsumer/SimpleProducer.java
@@ -8,11 +8,11 @@ import java.util.Properties;
 import kafka.javaapi.producer.Producer;
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
-//producerclass
+//producerclass comment
 public class SimpleProducer {
 	private static Producer<Integer, String> producer;
 	
-    private static final String topic= "testt"
+    private static final String topic= "testt";
     // init props
     public void initialize() {
           Properties producerProps = new Properties();
